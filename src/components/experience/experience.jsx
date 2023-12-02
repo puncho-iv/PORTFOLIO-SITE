6dbe466109329b4eeb84497e46613dbf8762c11a
puncho-iv <unicusdnl@gmail.com>
2023-12-02 02:20:35 +0000

    updates

--- src/components/experience/experience.jsx
+++ src/components/experience/experience.jsx
@@ -11,69 +11,69 @@ const Experience = () => {
       </div>
       <div className="container experience_container">
         <div className="experience_frontend">
-          <h3>Frontend & Backend Development</h3>
+          <h3>Frontend Development</h3>
           <div className="experience_content">
             <article className='experience_details'>
               <HiBadgeCheck className='experience_details-icon'/>
              <div>
                 <h4>HTML</h4>
-                <small className='text-light'>Intermediate</small>
+                {/* <small className='text-light'>Intermediate</small> */}
              </div>
             </article>
             <article className='experience_details'>
               <HiBadgeCheck className='experience_details-icon'/>
               <div>
                 <h4>CSS</h4>
-                <small className='text-light'>Intermediate</small>
+                {/* <small className='text-light'>Intermediate</small> */}
               </div>
             </article>
             <article className='experience_details'>
               <HiBadgeCheck className='experience_details-icon'/>
               <div>
                 <h4>JavaScript</h4>
-                <small className='text-light'>Proeficient</small>
+                {/* <small className='text-light'>Proeficient</small> */}
               </div>
             </article>
             <article className='experience_details'>
               <HiBadgeCheck className='experience_details-icon'/>
               <div>
                 <h4>Tailwind CSS</h4>
-                <small className='text-light'>Proeficient</small>
+                {/* <small className='text-light'>Proeficient</small> */}
               </div>
             </article>
             <article className='experience_details'>
               <HiBadgeCheck className='experience_details-icon'/>
               <div>
                 <h4>React.js</h4>
-                <small className='text-light'>Proeficient</small>
+                {/* <small className='text-light'>Proeficient</small> */}
               </div>
             </article>
             <article className='experience_details'>
               <HiBadgeCheck className='experience_details-icon'/>
               <div>
                 <h4>Next.js</h4>
-                <small className='text-light'>Proeficient</small>
+                {/* <small className='text-light'>Proeficient</small> */}
               </div>
             </article>
             <article className='experience_details'>
               <HiBadgeCheck className='experience_details-icon'/>
               <div>
                 <h4>React-Native</h4>
-                <small className='text-light'>Proeficient</small>
+                {/* <small className='text-light'>Proeficient</small> */}
               </div>
             </article>
             <article className='experience_details'>
               <HiBadgeCheck className='experience_details-icon'/>
               <div>
                 <h4>Express.js</h4>
-                <small className='text-light'>Proeficient</small>
+                {/* <small className='text-light'>Proeficient</small> */}
               </div>
             </article>
             <article className='experience_details'>
               <HiBadgeCheck className='experience_details-icon'/>
               <div>
                 <h4>Redux</h4>
-                <small className='text-light'>Proeficient</small>
+                {/* <small className='text-light'>Proeficient</small> */}
               </div>
             </article>
   
@@ -88,35 +88,28 @@ const Experience = () => {
               <HiBadgeCheck className='experience_details-icon'/>
               <div>
                 <h4>Illustrator</h4>
-                <small className='text-light'>Intermediate</small>
+                {/* <small className='text-light'>Intermediate</small> */}
               </div>
             </article>
             <article className='experience_details'>
               <HiBadgeCheck className='experience_details-icon'/>
               <div>
                  <h4>Photoshop</h4>
-                  <small className='text-light'>Intermediate</small>
-              </div>
-            </article>
-            <article className='experience_details'>
-              <HiBadgeCheck className='experience_details-icon'/>
-              <div>
-                <h4>After Effects</h4>
-                <small className='text-light'>Beginner</small>
+                  {/* <small className='text-light'>Intermediate</small> */}
               </div>
             </article>
             <article className='experience_details'>
               <HiBadgeCheck className='experience_details-icon'/>
               <div>
                 <h4>Priemiere Pro</h4>
-                <small className='text-light'>Beginner</small> 
+                {/* <small className='text-light'>Beginner</small>  */}
               </div> 
             </article>
             <article className='experience_details'>
               <HiBadgeCheck className='experience_details-icon'/>
               <div>
                 <h4>Figma</h4>
-                <small className='text-light'>Proeficient</small>
+                {/* <small className='text-light'>Proeficient</small> */}
               </div>
             </article>
          </div>
