bd6dff3fb4b6bc27ff2cd18a75d234140804d9f7
puncho-iv <unicusdnl@gmail.com>
2025-02-01 20:11:20 +0000

    updates

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import CV2 from '../../assets/CV2.pdf'
+import CV3 from '../../assets/CV3.pdf'
 import './header.css'
 
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href= {CV2} download className='btn'>Download CV</a>
+        <a href= {CV3} download className='btn'>Download CV</a>
         <a href="#contacts" className='btn btn-primary'>Design Now</a>        
     </div>
   )
